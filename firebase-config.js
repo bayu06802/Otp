36c08e32f6a5d158c0c02946b53fe8b6e95ad87e
bayu06802 <134147423+bayu06802@users.noreply.github.com>
2026-07-15 23:09:38 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Ganti dengan konfigurasi dari Step 6!
+const firebaseConfig = {
+    apiKey: "AIzaSyANEBuXf5qKiOVF2DR9ifyuphwkn0dkdio",
+    authDomain: "otp-bayu.firebaseapp.com",
+    projectId: "otp-bayu",
+    storageBucket: "otp-bayu.firebasestorage.app",
+    messagingSenderId: "309880884423",
+    appId: "1:309880884423:web:b33aa90f79272033ab82e2"
+};
